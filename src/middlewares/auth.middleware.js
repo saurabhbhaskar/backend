58dd9df30b04c7c55d82886915448fee71152b08
Saurabh Bhaskar <saurabhbhaskar199@gmail.com>
2024-03-23 12:43:16 +0530

    configured login and logout controllers

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,39 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import {User} from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {     // here we don't use res so we add _
+    try {
+    // request have cookie access so we are going to take tokens from the cookie
+    // here we also use header because on apps user send authorization key through headers
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if(!token){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    // in User model we sent you can see we in generateAccessToken method we sent id, username, email, ... in access token
+    // we have to decode this info
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+    if(!user){
+        throw new ApiError(401, "Invalid Access Token")
+    }
+
+    req.user = user;
+    
+    next()
+
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+
+})
+
+
+
+
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logoutUser, loginUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
@@ -17,10 +18,14 @@ router.route("/register").post(
         }
     ]),
     registerUser
-    )
+)
 
 
-// router.route("/login").post(login)
+router.route("/login").post(loginUser)
+
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 
 export default router;
