da7f8fbe0d47478987f10ef8e764bd0ac2e618fa
Saurabh Bhaskar <saurabhbhaskar199@gmail.com>
2024-03-30 15:14:23 +0530

    add comment controllers

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 import videoRouter from "./routes/video.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 
 
@@ -27,6 +28,7 @@ import likeRouter from "./routes/like.routes.js";
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/comment", commentRouter);
 
 
 
