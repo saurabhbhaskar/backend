398a2a79c03841daefc63b91e856ccf8ea8feb0c
Saurabh Bhaskar <saurabhbhaskar199@gmail.com>
2024-04-02 08:38:12 +0530

    add subscription controller

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import userRouter from './routes/user.routes.js'
 import videoRouter from "./routes/video.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 
 
@@ -29,6 +30,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 
 
