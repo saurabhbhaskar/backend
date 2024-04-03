c3f2415f7daf79ab49c157f612d7627efb9ac794
Saurabh Bhaskar <saurabhbhaskar199@gmail.com>
2024-04-03 22:50:03 +0530

    add tweet controller

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import videoRouter from "./routes/video.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 
 
@@ -31,6 +32,7 @@ app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
 
 
 
